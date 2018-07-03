0ae3b5e3dc99825cd31e7161016d7db3ffca02da
mikong <michaelgalero@gmail.com>
2018-07-03 22:56:48 +0800

    Implement simple key-value store REPL.
    Co-authored-by: mikong <4162+mikong@users.noreply.github.com>
    Co-authored-by: Mithi Sevilla <1670421+mithi@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+use std::io;
+use std::io::Write;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut data = HashMap::new();
+
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut command = String::new();
+
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+
+        process_command(&mut data, &command);
+    }
+}
+
+fn process_command(data: &mut HashMap<String, Vec<u8>>, command: &str) {
+    let v: Vec<&str> = command.split_whitespace().collect();
+
+    let command = v[0];
+
+    match command {
+
+        "SET" if v.len() == 3 => {
+            data.insert(v[1].to_string(), v[2].as_bytes().to_vec());
+            println!("OK");
+        },
+
+
+        "GET" if v.len() == 2 => {
+            if let Some(value) = data.get(v[1]) {
+                println!("\"{}\"", String::from_utf8_lossy(value));
+            } else {
+                println!("ERROR: KEY NOT FOUND");
+            }
+        },
+
+        "DELETE" if v.len() == 2 => {
+            if let Some(_) = data.remove(v[1]) {
+                println!("OK");
+            } else {
+                println!("ERROR: KEY NOT FOUND");
+            }
+        },
+
+        "EXISTS" if v.len() == 2 => {
+            if data.contains_key(v[1]) {
+                println!("1");
+            } else {
+                println!("0");
+            }
+        },
+
+        _ => {
+            println!("Command not recognized. Try again");
+        },
+    }
 }
