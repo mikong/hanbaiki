515aed93cb0d2807936b9deb45c75364fc97e3af
mikong <michaelgalero@gmail.com>
2018-07-07 21:03:17 +0800

    Replace REPL with a basic TCP server.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
 use std::io;
-use std::io::Write;
+use std::io::{Write, Read};
 use std::collections::HashMap;
+
+use std::net::TcpListener;
 
 fn main() {
     let mut data = HashMap::new();
 
+    data_server("127.0.0.1:6363", &mut data).expect("error: ");
+}
+
+fn data_server(addr: &str, data: &mut HashMap<String, Vec<u8>>) -> io::Result<()> {
+    let listener = TcpListener::bind(addr)?;
+    println!("listening on {}", addr);
+
     loop {
-        print!("> ");
-        io::stdout().flush().unwrap();
+        // Wait for a client to connect.
+        let (mut stream, addr) = listener.accept()?;
+        println!("connection received from {}", addr);
 
-        let mut command = String::new();
+        let mut write_stream = stream.try_clone()?;
 
-        io::stdin()
-            .read_line(&mut command)
-            .expect("Failed to read line");
+        // Read command.
+        let mut buf = vec![];
+        stream.read_to_end(&mut buf)?;
+        let command = String::from_utf8(buf).unwrap();
+        let response = process_command(data, &command);
 
-        process_command(&mut data, &command);
+        // Return response.
+        write_stream.write(response.as_bytes())?;
+        println!("connection closed");
     }
 }
 
-fn process_command(data: &mut HashMap<String, Vec<u8>>, command: &str) {
+fn process_command(data: &mut HashMap<String, Vec<u8>>, command: &str) -> String {
     let v: Vec<&str> = command.split_whitespace().collect();
 
     let command = v[0];
@@ -28,36 +42,36 @@ fn process_command(data: &mut HashMap<String, Vec<u8>>, command: &str) {
 
         "SET" if v.len() == 3 => {
             data.insert(v[1].to_string(), v[2].as_bytes().to_vec());
-            println!("OK");
+            "OK".to_string()
         },
 
 
         "GET" if v.len() == 2 => {
             if let Some(value) = data.get(v[1]) {
-                println!("\"{}\"", String::from_utf8_lossy(value));
+                String::from_utf8_lossy(value).into_owned()
             } else {
-                println!("ERROR: KEY NOT FOUND");
+                "ERROR: KEY NOT FOUND".to_string()
             }
         },
 
         "DELETE" if v.len() == 2 => {
             if let Some(_) = data.remove(v[1]) {
-                println!("OK");
+                "OK".to_string()
             } else {
-                println!("ERROR: KEY NOT FOUND");
+                "ERROR: KEY NOT FOUND".to_string()
             }
         },
 
         "EXISTS" if v.len() == 2 => {
             if data.contains_key(v[1]) {
-                println!("1");
+                "1".to_string()
             } else {
-                println!("0");
+                "0".to_string()
             }
         },
 
         _ => {
-            println!("Command not recognized. Try again");
+            "Command not recognized. Try again".to_string()
         },
     }
 }
