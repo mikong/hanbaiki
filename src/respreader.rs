9778ac82ee99a0481a65af9d158af2d1d4cb5ad2
mikong <michaelgalero@gmail.com>
2018-07-15 23:02:02 +0800

    Handle bulk string in RESP reader.

--- src/respreader.rs
+++ src/respreader.rs
@@ -22,7 +22,7 @@ impl RespReader {
         match type_buf[0] {
             b'+' | b'-' => self.get_simple_message(stream)?,
             b':' => self.get_integer(stream)?,
-            // TODO: b'$' => ,
+            b'$' => self.get_bulk_string(stream)?,
             // TODO: b'*' => ,
             _ => return Err("Invalid RESP type".to_string()),
         }
@@ -72,4 +72,88 @@ impl RespReader {
         }
     }
 
+    fn get_bulk_string(&mut self, stream: &mut TcpStream) -> Result<(), String> {
+        let mut buf = vec![0; 20];
+        let mut state = BulkStringState::GetSize;
+        let mut size = 0;
+
+        let mut index = self.message.len();
+
+        loop {
+            let length = stream.read(&mut buf).unwrap();
+
+            if length == 0 {
+                return Err("EOF before end of frame".to_string());
+            }
+
+            for byte in buf[0..length].iter() {
+                self.message.push(*byte)
+            }
+
+            if state == BulkStringState::GetSize {
+                if let Some(i) = self.find_break(index) {
+                    match self.parse_int(i) {
+                        Some(n) => {
+                            size = n;
+                            state = BulkStringState::CheckEOL;
+                            index = i + 1;
+                        },
+                        None => return Err("Not an integer".to_string()),
+                    }
+                } else {
+                    index = self.message.len();
+                }
+            }
+
+            if state == BulkStringState::CheckEOL {
+                if let Some(byte) = self.message.get(index) {
+                    if *byte == b'\n' {
+                        state = BulkStringState::BuildString;
+                        index += 1;
+                    } else {
+                        return Err("CR not followed by LF".to_string());
+                    }
+                }
+            }
+
+            if state == BulkStringState::BuildString {
+                if self.message.len() > index + size as usize {
+                    index += size as usize;
+                    if self.message[index] == b'\r' && self.message[index + 1] == b'\n' {
+                        return Ok(());
+                    } else {
+                        return Err("Does not end with CRLF".to_string());
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    fn find_break(&self, start_index: usize) -> Option<usize> {
+        if let Some(slice) = self.message.get(start_index..) {
+            for (i, byte) in slice.iter().enumerate() {
+                if *byte == b'\r' {
+                    return Some(start_index + i);
+                }
+            }
+        }
+        None
+    }
+
+    fn parse_int(&self, end_index: usize) -> Option<i64> {
+        match str::from_utf8(&self.message[1..end_index]) {
+            Ok(s) => s.parse::<i64>().ok(),
+            Err(_) => None,
+        }
+    }
+
+}
+
+#[derive(Debug, PartialEq)]
+enum BulkStringState {
+    GetSize,
+    CheckEOL,
+    BuildString,
 }
