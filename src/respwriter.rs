85afb137cdef3a8a6479aa72f5df8cca4fa2b8ab
mikong <michaelgalero@gmail.com>
2018-07-16 22:19:49 +0800

    Remove pub keyword in RESP writer tests.

--- src/respwriter.rs
+++ src/respwriter.rs
@@ -49,24 +49,24 @@ mod test {
     use super::RespWriter;
 
     #[test]
-    pub fn check_simple_string() {
+    fn check_simple_string() {
         assert_eq!("+OK\r\n", RespWriter::to_simple_string("OK").unwrap());
         assert!(RespWriter::to_simple_string("PING\r\n...").is_err());
     }
 
     #[test]
-    pub fn check_error() {
+    fn check_error() {
         assert_eq!("-ERR\r\n", RespWriter::to_error("ERR").unwrap());
         assert!(RespWriter::to_error("PONG\r\n...").is_err());
     }
 
     #[test]
-    pub fn check_integer() {
+    fn check_integer() {
         assert_eq!(":100\r\n", RespWriter::to_integer(100));
     }
 
     #[test]
-    pub fn check_bulk_string() {
+    fn check_bulk_string() {
         assert_eq!("$0\r\n\r\n", RespWriter::to_bulk_string(""));
         assert_eq!("$6\r\nfoobar\r\n", RespWriter::to_bulk_string("foobar"));
         assert_eq!("$2\r\n\r\n\r\n", RespWriter::to_bulk_string("\r\n"));
@@ -74,12 +74,12 @@ mod test {
     }
 
     #[test]
-    pub fn check_null_bulk_string() {
+    fn check_null_bulk_string() {
         assert_eq!("$-1\r\n", RespWriter::null_bulk_string());
     }
 
     #[test]
-    pub fn check_array() {
+    fn check_array() {
         let v = vec!["foo", "bar"];
         assert_eq!("*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n", RespWriter::to_array(&v));
     }
