ffdc33e413e4bca40ef2d10914adbc76b05d971d
mikong <michaelgalero@gmail.com>
2018-08-06 23:50:40 +0800

    Iterate over incoming connections. Use Arc+RwLock for concurrency.

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,39 @@ use std::io;
 use std::io::Write;
 use std::collections::HashMap;
 
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::sync::{RwLock, Arc};
 
 use hanbaiki::{RespWriter, RespReader};
 use hanbaiki::Value;
 
-fn main() {
-    let mut data = HashMap::new();
+type KvStore = Arc<RwLock<HashMap<String, Vec<u8>>>>;
 
-    data_server("127.0.0.1:6363", &mut data).expect("error: ");
+fn main() {
+    let data = Arc::new(RwLock::new(HashMap::new()));
+
+    data_server("127.0.0.1:6363", data);
 }
 
-fn data_server(addr: &str, data: &mut HashMap<String, Vec<u8>>) -> io::Result<()> {
-    let listener = TcpListener::bind(addr)?;
+fn data_server(addr: &str, data: KvStore) {
+    let listener = TcpListener::bind(addr).unwrap();
     println!("listening on {}", addr);
 
-    // Wait for a client to connect.
-    let (mut stream, addr) = listener.accept()?;
-    println!("connection received from {}", addr);
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let data = Arc::clone(&data);
+                thread::spawn(move || {
+                    handle_client(stream, data)
+                });
+            },
+            Err(e) => println!("connection failed: {:?}", e),
+        }
+    }
+}
 
+fn handle_client(mut stream: TcpStream, data: KvStore) -> io::Result<()> {
     let mut write_stream = stream.try_clone()?;
     write_stream.set_nodelay(true)?;
 
@@ -32,6 +46,7 @@ fn data_server(addr: &str, data: &mut HashMap<String, Vec<u8>>) -> io::Result<()
         reader.frame_message(&mut stream).unwrap();
         let command = reader.value;
 
+        let data = Arc::clone(&data);
         let response = process_command(data, command);
 
         // Return response.
@@ -39,7 +54,7 @@ fn data_server(addr: &str, data: &mut HashMap<String, Vec<u8>>) -> io::Result<()
     }
 }
 
-fn process_command(data: &mut HashMap<String, Vec<u8>>, command: Value) -> String {
+fn process_command(data: KvStore, command: Value) -> String {
     let mut v = match command {
         Value::Array(values) => values,
         _ => panic!("Expected command to be Value::Array"),
@@ -50,12 +65,14 @@ fn process_command(data: &mut HashMap<String, Vec<u8>>, command: Value) -> Strin
     match command.as_ref() {
 
         "SET" if v.len() == 3 => {
+            let mut data = data.write().unwrap();
             data.insert(v[1].take().to_string(), v[2].take().to_string().into_bytes());
             RespWriter::to_simple_string("OK").unwrap()
         },
 
 
         "GET" if v.len() == 2 => {
+            let data = data.read().unwrap();
             if let Some(value) = data.get(&v[1].take().to_string()) {
                 let value = String::from_utf8_lossy(value).into_owned();
                 RespWriter::to_bulk_string(&value)
@@ -65,6 +82,7 @@ fn process_command(data: &mut HashMap<String, Vec<u8>>, command: Value) -> Strin
         },
 
         "DELETE" if v.len() == 2 => {
+            let mut data = data.write().unwrap();
             if let Some(_) = data.remove(&v[1].take().to_string()) {
                 RespWriter::to_simple_string("OK").unwrap()
             } else {
@@ -73,6 +91,7 @@ fn process_command(data: &mut HashMap<String, Vec<u8>>, command: Value) -> Strin
         },
 
         "EXISTS" if v.len() == 2 => {
+            let data = data.read().unwrap();
             if data.contains_key(&v[1].take().to_string()) {
                 RespWriter::to_integer(1)
             } else {
